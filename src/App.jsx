a4fe67d11381eaa56593756ca5f37270a84c6d98
Anand S <anandsatheeshk@gmail.com>
2023-04-10 10:00:42 +0530

    text in textarea is displayed on preview div

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import React from "react";
+import './App.css' 
+
 export default function App() {
     return (
-        <div>Hello World</div>
+        <Previewer />
     );
+}
+
+class Previewer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            markdown: ''
+        }
+    }
+    updatePreview = (event) => {
+        this.setState({
+            markdown: event.target.value
+        })
+    }
+    render() {
+        return (
+            <div>
+                <textarea 
+                    name="editor" 
+                    id="editor" 
+                    cols="30" 
+                    rows="10"
+                    onChange={this.updatePreview}
+                    value={this.state.markdown}></textarea>
+                <div id="preview">{this.state.markdown}</div>
+            </div>
+        );
+    }
 }
